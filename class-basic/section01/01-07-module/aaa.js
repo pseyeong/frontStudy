ca851ade687109e561ad1d840f9e6a971fcba67c
pseyeong <yeong.psy@gmail.com>
2026-05-06 17:51:32 +0900

    모듈설치

---
+++ class-basic/section01/01-07-module/aaa.js
@@ -0,0 +1,9 @@
+import 좋은과일, { 포도 } from './bbb.js'
+
+console.log(좋은과일, 포도);
+
+import * as 과일뭉치 from './bbb.js'
+
+console.log(과일뭉치.사과);
+console.log(과일뭉치.포도);
+console.log(과일뭉치.default);
---
+++ class-basic/section02/02-02-cra2/myproject/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button>dd</button>
+    </div>
+  );
+}
+
+export default App;
